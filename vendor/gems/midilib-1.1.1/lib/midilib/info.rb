a4d167f05d09a03ce9c707a2f8a60374faf22409
Jim Menard <jim@10gen.com>
2008-12-23 10:59:09 -0500

    Added a frozen gem to make sure we can refer to it

---
+++ vendor/gems/midilib-1.1.1/lib/midilib/info.rb
@@ -0,0 +1,9 @@
+module MIDI
+
+    VERSION_MAJOR = 1
+    VERSION_MINOR = 1
+    VERSION_TWEAK = 1
+    Version = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_TWEAK}"
+    Copyright = 'Copyright (c) 2003-2007 by Jim Menard <jimm@io.com>'
+
+end
